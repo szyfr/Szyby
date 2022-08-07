7269db3474d937fae95ae647340cdb8e7b5d5fbc
Szyfr <jmanz5@hotmail.com>
2022-08-07 12:04:43 -0400

    Main loop working, Can draw to screen, and basic event handler

--- src/main.go
+++ src/main.go
@@ -3,7 +3,8 @@ package main
 
 import (
 	_"runtime"
-	_"fmt"
+	"fmt"
+	"time"
 	"image/color"
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -12,45 +13,57 @@ import (
 type Program struct {
 	window  *sdl.Window
 	surface *sdl.Surface
+	running  bool
 }
+
+
+const MOD = 8
+
+
+var program *Program
 
 
 func main() {
-	program := init_sdl()
+	program = init_sdl()
 
-	program.surface.FillRect(nil, sdl.MapRGB(program.surface.Format,0xFF,0xFF,0xFF))
-	program.window.UpdateSurface()
+	for program.running {
+		event_handler()
 
-//	test := program.surface.BytesPerPixel()
-//	fmt.Printf("%i\n",test)
+		rect0 := sdl.Rect{896,   0,  384, 720}
+		rect1 := sdl.Rect{0  , 704, 1280,  16}
+		program.surface.FillRect(nil,    sdl.MapRGB(program.surface.Format,0xFF,0xFF,0xFF))
+		program.surface.FillRect(&rect0, sdl.MapRGB(program.surface.Format,200,200,200))
+		program.surface.FillRect(&rect1, sdl.MapRGB(program.surface.Format,200,200,200))
+	
+		t0 := time.Now()
+		for o:=0;o<11*8;o+=1 {
+			for i:=0;i<14*8;i+=1 { draw_pixel(i, o, color.RGBA{0x00,0x00,0x00,0xFF}) }
+		}
+		t1 := time.Since(t0).Milliseconds()
+		fmt.Printf("%v ms\n", t1)
 
-	program.surface.Set(0,0,color.Color{0xFFFF,0xFFFF,0xFFFF,0xFFFF})
+		program.window.UpdateSurface()
+	}
 
-	sdl.Delay(2000)
-
-	program.window.Destroy()
-	sdl.Quit()
+	close_sdl()
 }
 
 
-func init_sdl() *Program {
-	program := new(Program)
+func event_handler() {
+	for e := sdl.PollEvent(); e != nil; e = sdl.PollEvent() {
+		switch e.(type) {
+		case *sdl.QuitEvent:
+			program.running = false
+			return
+		}
+	}
+}
 
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil { panic(err) }
 
-	window, err := sdl.CreateWindow(
-		"TEST",
-		sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED,
-		1280, 720,
-		sdl.WINDOW_SHOWN,
-	)
-	if err != nil { panic(err) }
-	program.window = window
-
-	surface, err := program.window.GetSurface()
-	if err != nil { panic(err) }
-	program.surface = surface
-
-	return program
+func draw_pixel(x,y int, c color.Color) {
+	for o:=0;o<MOD;o+=1 {
+		for i:=0;i<MOD;i+=1 {
+			program.surface.Set(x*MOD+i, y*MOD+o, c)
+		}
+	}
 }
